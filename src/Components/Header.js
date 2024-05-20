61c3b7da04832c73be2bd489ffad018865c1d313
Sagar Panchal <forpcsagar055@gmail.com>
2024-05-20 15:56:02 +0530

    changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     if (this.props.data) {
-      var firstName = this.props.data.firstName
       var name = this.props.data.name
       var occupation = this.props.data.occupation
       var description = this.props.data.description
@@ -11,7 +10,7 @@ class Header extends Component {
       var networks = this.props.data.social.map(function (network) {
         return (
           <li key={network.name}>
-            <a href={network.url} target='_blank'>
+            <a href={network.url} target='_blank' rel="noopener noreferrer">
               <i className={network.className}></i>
             </a>
           </li>
