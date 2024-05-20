f944543c2b46105192e26bcccf3e74f261f9ef40
Sagar Panchal <forpcsagar055@gmail.com>
2024-05-20 16:01:29 +0530

    Normal changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -26,13 +26,7 @@ class Footer extends Component {
 
             <ul className='copyright'>
               <li>
-                &copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker
-              </li>
-              <li>
-                Design by{' '}
-                <a title='Styleshout' href='http://www.styleshout.com/'>
-                  Styleshout
-                </a>
+                Copyright 2024
               </li>
             </ul>
           </div>
